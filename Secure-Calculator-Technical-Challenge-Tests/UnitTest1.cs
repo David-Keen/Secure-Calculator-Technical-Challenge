9701bdd78bcf9f5bfee57b7a486ce83f4fdf2c67
David Keen <davidkeen972@icloud.com>
2020-02-20 12:05:41 +0000

    Initial Commit

---
+++ Secure-Calculator-Technical-Challenge-Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Secure_Calculator_Technical_Challenge_Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
