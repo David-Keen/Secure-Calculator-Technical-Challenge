c6aabb6507fd96597120a0d29d88d4303d9e5c2f
David Keen <davidkeen972@icloud.com>
2020-02-20 15:37:46 +0000

    Added subtraction tests

---
+++ Secure-Calculator-Technical-Challenge-Tests/CalculatorSubtractionTests.cs
@@ -0,0 +1,57 @@
+using NUnit.Framework;
+
+namespace Secure_Calculator_Technical_Challenge_Tests
+{
+    public class CalculatorSubtractionTests
+    {
+        Secure_Calculator_Technical_Challenge.Calculator.ICalculator Calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            Calculator = new Secure_Calculator_Technical_Challenge.Calculator.BasicCalculator();
+        }
+
+        [Test]
+        public void OneAndOneShouldBeZero()
+        {
+            var sum = Calculator.Subtract(1, 1);
+            Assert.AreEqual(sum, 0);
+        }
+
+        [Test]
+        public void FiveAndTwoShouldBeThree()
+        {
+            var sum = Calculator.Subtract(5, 2);
+            Assert.AreEqual(sum, 3);
+        }
+
+        [Test]
+        public void MinusThreeAndOneShouldBeMinusFour()
+        {
+            var sum = Calculator.Subtract(-3, 1);
+            Assert.AreEqual(sum, -4);
+        }
+
+        [Test]
+        public void OneAndMinusThreeShouldBeFour()
+        {
+            var sum = Calculator.Subtract(1, -3);
+            Assert.AreEqual(sum, 4);
+        }
+
+        [Test]
+        public void MinusThreeAndMinusThreeShouldBeZero()
+        {
+            var sum = Calculator.Subtract(-3, -3);
+            Assert.AreEqual(sum, 0) ;
+        }
+
+        [Test]
+        public void ZeroAndZeroShouldBeZero()
+        {
+            var sum = Calculator.Subtract(0, 0);
+            Assert.AreEqual(sum, 0);
+        }
+    }
+}
