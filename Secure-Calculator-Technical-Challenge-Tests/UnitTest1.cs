1ffd8f34ef6067cfb63d08782e91f90d32b1d6dc
David Keen <davidkeen972@icloud.com>
2020-02-20 13:37:35 +0000

    Added test decorators

--- Secure-Calculator-Technical-Challenge-Tests/UnitTest1.cs
+++ Secure-Calculator-Technical-Challenge-Tests/UnitTest1.cs
@@ -20,30 +20,35 @@ namespace Secure_Calculator_Technical_Challenge_Tests
             Assert.AreEqual(sum, 2);
         }
 
+        [Test]
         public void TwoAndThreeShouldBeFive()
         {
             var sum = Calculator.Add(2, 3);
             Assert.AreEqual(sum, 5);
         }
 
+        [Test]
         public void MinusThreeAndOneShouldBeMinusTwo()
         {
             var sum = Calculator.Add(-3, 1);
             Assert.AreEqual(sum, 1);
         }
 
+        [Test]
         public void OneAndMinusThreeShouldBeMinusTwo()
         {
             var sum = Calculator.Add(1, -3);
             Assert.AreEqual(sum, -2);
         }
 
+        [Test]
         public void MinusThreeAndMinusThreeShouldBeMinusSix()
         {
             var sum = Calculator.Add(-3, -3);
             Assert.AreEqual(sum, -6) ;
         }
 
+        [Test]
         public void ZeroAndZeroShouldBeZero()
         {
             var sum = Calculator.Add(0, 0);
