ae6e13c1ce21fae8e959f8de36e86ff3ae47baa7
David-Keen <davidkeen972@icloud.com>
2020-02-22 15:23:25 +0000

    Fixed compatiblity issues by removing accessor value

--- Calculator Service/Calculator/ICalculator.cs
+++ Calculator Service/Calculator/ICalculator.cs
@@ -6,9 +6,9 @@ namespace Secure_Calculator_Technical_Challenge.Calculator
 {
     public interface ICalculator
     {
-        public double Add(double lhs, double rhs);
-        public double Subtract(double lhs, double rhs);
-        public double Multiply(double lhs, double rhs);
-        public double Divide(double lhs, double rhs);
+        double Add(double lhs, double rhs);
+        double Subtract(double lhs, double rhs);
+        double Multiply(double lhs, double rhs);
+        double Divide(double lhs, double rhs);
     }
 }
