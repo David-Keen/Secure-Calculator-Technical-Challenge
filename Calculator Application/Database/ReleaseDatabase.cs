c936298dfc1a65ab6884a4d1a6a062e2e83ae814
David Keen <davidkeen972@icloud.com>
2020-02-29 18:42:51 +0000

    Refactored ReleaseDatabase

--- Calculator Application/Database/ReleaseDatabase.cs
+++ Calculator Application/Database/ReleaseDatabase.cs
@@ -14,34 +14,63 @@ namespace Calculator_Application.Database
             return null;
         }
 
-        User.IUser IDatabaseContext.GetUser(int id)
+        private SQLiteConnection OpenDatabaseConnection()
         {
             var path = System.IO.Directory.GetCurrentDirectory() + "\\database.sqlite3";
             var connection = new SQLiteConnection(String.Format("Data Source={0};Version=3", path));
-            System.Diagnostics.Debug.WriteLine(String.Format("Path is: {0}", path));
-            var command = new SQLiteCommand();
-            command.CommandText = "SELECT * FROM users WHERE Id = @Id";
-            var idParam = new SQLiteParameter("@Id", id);
-            command.Parameters.Add(idParam);
-
             connection.Open();
+            System.Diagnostics.Debug.WriteLine(String.Format("Path is: {0}", path));
+            return connection;
+        }
+
+        private void PrepareCommand(ref SQLiteCommand command, SQLiteParameter[] parameters, ref SQLiteConnection connection)
+        {
+            foreach (var parameter in parameters)
+            {
+                command.Parameters.Add(parameter);
+            }
             command.Connection = connection;
             command.Prepare();
-            System.Diagnostics.Debug.WriteLine("Table rows: {0}", command.CommandText);
+        }
+
+        private SQLiteCommand ExecuteQuery(string query, SQLiteParameter[] parameters, ref SQLiteConnection connection)
+        {
+            var command = new SQLiteCommand();
+            command.CommandText = query;
+            PrepareCommand(ref command, parameters, ref connection);
+            return command;
+        }
+
+        User.IUser IDatabaseContext.GetUser(int id)
+        {
+            var connection = OpenDatabaseConnection();
+            SQLiteParameter[] parameters = { new SQLiteParameter("@Id", id) };
+            var command = ExecuteQuery("SELECT * FROM users WHERE Id = @Id", parameters, ref connection);
+
             using (var reader = command.ExecuteReader())
             {
-                var table = new DataTable();
-                table.Load(reader);
-                reader.Close();
+                var table = LoadTable(reader);
                 connection.Close();
-                System.Diagnostics.Debug.WriteLine("Number of rows: {0}", table.Rows.Count);
-                if (table.Rows.Count <= 0) return null;
-                var row = table.Rows[0];
-                string firstName = row["FirstName"].ToString();
-                string lastName = row["LastName"].ToString();
-                string passwordHash = row["Password"].ToString();
-                return new User.User(firstName, lastName, passwordHash, id);
+                reader.Close();
+                return GetUserFromQueryResult(table, id);
             }
+        }
+
+        private DataTable LoadTable(SQLiteDataReader reader)
+        {
+            var table = new DataTable();
+            table.Load(reader);
+            return table;
+        }
+
+        private User.User GetUserFromQueryResult(DataTable table, int id)
+        {
+            if (table.Rows.Count <= 0) return null;
+            var row = table.Rows[0];
+            string firstName = row["FirstName"].ToString();
+            string lastName = row["LastName"].ToString();
+            string passwordHash = row["Password"].ToString();
+            return new User.User(firstName, lastName, passwordHash, id);
         }
     }
 }
