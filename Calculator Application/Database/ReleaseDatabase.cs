0e45a7eaef14935c3eab257eb6654e2975dc3a2b
David Keen <davidkeen972@icloud.com>
2020-02-29 17:53:38 +0000

    Renamed namespace to reflect filepath change

--- Calculator Application/Database/ReleaseDatabase.cs
+++ Calculator Application/Database/ReleaseDatabase.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Data;
 using System.Data.SQLite;
 
-namespace Database
+namespace Calculator_Application.Database
 {
     public class ReleaseDatabase : IDatabaseContext
     {
--- Calculator Application/Database/TestDatabase.cs
+++ Calculator Application/Database/TestDatabase.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Database
+namespace Calculator_Application.Database
 {
     public class TestDatabase : IDatabaseContext
     {
